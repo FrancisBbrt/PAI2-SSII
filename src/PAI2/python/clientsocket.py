0b3f0d5b579b36b5f10be0088c63084f641efaac
madgIitch <peorrr9@gmail.com>
2023-10-15 19:20:26 +0200

    noncedb and mac

--- src/PAI2/python/clientsocket.py
+++ src/PAI2/python/clientsocket.py
@@ -1,39 +1,72 @@
+# Importar las bibliotecas necesarias
 import socket
 import json
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, hmac
 
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 3030  # The port used by the server
+# Definir el HOST y el PORT para la conexión del cliente al servidor
+HOST = "127.0.0.1"
+PORT = 3030
 
+# Clave secreta utilizada para generar y verificar el HMAC
+SECRET_KEY = b"this_is_a_super_secret_key"
+
+# Función para generar el HMAC de los datos
+def generate_hmac(data):
+    # Iniciar el HMAC con la clave secreta y el algoritmo SHA256
+    h = hmac.HMAC(SECRET_KEY, hashes.SHA256(), backend=default_backend())
+    # Actualizar el HMAC con los datos que queremos autenticar
+    h.update(data.encode())
+    # Finalizar y retornar el HMAC en formato hexadecimal
+    return h.finalize().hex()
+
+# Establecer conexión con el servidor
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    
 
-    
-    while True:  # Infinite loop to keep client running and sending data
-        
-        # Enviar una solicitud de nonce inmediatamente después de establecer la conexión
-        nonce_request = "nonce_peticion"
-        s.sendall(nonce_request.encode())
+    # Bucle principal para enviar y recibir mensajes del servidor
+    while True:
+        try:
+            # Solicitar nonce al servidor
+            nonce_request = "nonce_peticion"
+            s.sendall(nonce_request.encode())
 
-        # Esperar y recibir el nonce del servidor
-        nonce_response = s.recv(1024).decode()
-        origen = 1234567887654321
-        destino = input("Introduce el numero de cuenta destino: ")
-        cantidad = input("Introduce la cantidad a transferir: ")
-        message = (origen, destino, cantidad)
-        
-        if message.lower() == "exit":
+            # Esperar la respuesta del servidor con el nonce
+            nonce_response = s.recv(1024).decode()
+
+            # Recoger datos de entrada del usuario para la transacción
+            origen = 1234567887654321
+            destino = input("Introduce el numero de cuenta destino: ")
+            
+            # Si el usuario introduce "exit", se sale del bucle
+            if destino.lower() == "exit":
+                break
+
+            cantidad = input("Introduce la cantidad a transferir: ")
+            message = (origen, destino, cantidad)
+
+            # Crear un objeto con el mensaje y el nonce para enviar al servidor
+            data_object = {
+                'message': message,
+                'nonce': nonce_response
+            }
+
+            # Convertir el objeto a un string JSON y generar el HMAC para ese mensaje
+            json_message = json.dumps(data_object)
+            mac = generate_hmac(json_message)
+
+            # Añadir el HMAC al objeto antes de enviarlo al servidor
+            data_object['hmac'] = mac
+            s.sendall(json.dumps(data_object).encode())
+
+            # Esperar la respuesta del servidor y mostrarla
+            data = s.recv(1024)
+            print(f"Received from server: {data.decode()}")
+
+        # Manejar cualquier excepción que pueda surgir y mostrar el error
+        except Exception as e:
+            print(f"Error: {e}")
             break
 
-        # Combinar el mensaje y el nonce en un objeto JSON
-        data_object = {
-            'message': message,
-            'nonce': nonce_response
-        }
-        json_message = json.dumps(data_object)
-        s.sendall(json_message.encode())
-        
-        data = s.recv(1024)
-        print(f"Received from server: {data.decode()}")
-
+# Indicar que el cliente ha finalizado su ejecución
 print("Client has exited.")
--- src/PAI2/python/serversocket.py
+++ src/PAI2/python/serversocket.py
@@ -1,35 +1,109 @@
+# Importar las bibliotecas necesarias
 import socket
 import threading
 import secrets
 import sqlite3
 import json
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, hmac
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 3030  # Port to listen on (non-privileged ports are > 1023)
+# Definir el HOST y el PORT para escuchar conexiones entrantes
+HOST = "127.0.0.1"
+PORT = 3030
 
-# Crear e inicializar la base de datos
+# Clave secreta utilizada para generar y verificar el HMAC
+SECRET_KEY = b"this_is_a_super_secret_key"
+
+# Inicializar conexión con la base de datos y crear tabla si no existe
 conn = sqlite3.connect('nonces.db')
 cursor = conn.cursor()
-cursor.execute('''CREATE TABLE IF NOT EXISTS nonces (nonce TEXT)''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS nonces (
+    nonce TEXT,
+    count INTEGER
+)''')
 conn.commit()
 
+# Función para generar un nonce único
 def generate_nonce():
-    """Function to generate a random nonce."""
-    nonce = secrets.token_hex(16)
+    while True:
+        nonce = secrets.token_hex(16)
+        cursor.execute("SELECT count FROM nonces WHERE nonce=?", (nonce,))
+        if cursor.fetchone() is None:
+            break
 
     # Guardar el nonce en la base de datos
     with conn:
-        cursor.execute("INSERT INTO nonces (nonce) VALUES (?)", (nonce,))
+        cursor.execute("INSERT INTO nonces (nonce, count) VALUES (?, ?)", (nonce, 0))
     return nonce
 
-def check_nonce_in_db(nonce):
-    """Check if a nonce exists in the database."""
-    cursor.execute("SELECT nonce FROM nonces WHERE nonce=?", (nonce,))
+# Función para verificar y actualizar el nonce en la base de datos
+def check_and_update_nonce(nonce):
+    cursor.execute("SELECT count FROM nonces WHERE nonce=?", (nonce,))
     result = cursor.fetchone()
-    return result is not None
+    if result:
+        count = result[0]
+        if count == 0:
+            with conn:
+                cursor.execute("UPDATE nonces SET count=? WHERE nonce=?", (1, nonce))
+            return True
+        else:
+            return False
+    else:
+        return False
 
+# Función para verificar el HMAC recibido
+def verify_hmac(data, received_hmac):
+    h = hmac.HMAC(SECRET_KEY, hashes.SHA256(), backend=default_backend())
+    h.update(data.encode())
+    try:
+        h.verify(bytes.fromhex(received_hmac))
+        return True
+    except:
+        return False
+
+# Función para manejar la comunicación con un cliente
 def handle_client(conn, addr):
-    """Function to handle communication with a client."""
+    # Crear una nueva conexión y cursor específicamente para este hilo
+    conn_db = sqlite3.connect('nonces.db')
+    cursor = conn_db.cursor()
+    
+    # Asegurarse de que la tabla exista
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS nonces (
+        nonce TEXT,
+        count INTEGER
+    )''')
+    conn_db.commit()
+
+    # Generar un nonce único
+    def generate_nonce():
+        while True:
+            nonce = secrets.token_hex(16)
+            cursor.execute("SELECT count FROM nonces WHERE nonce=?", (nonce,))
+            if cursor.fetchone() is None:
+                break
+
+        # Guardar el nonce en la base de datos
+        with conn_db:
+            cursor.execute("INSERT INTO nonces (nonce, count) VALUES (?, ?)", (nonce, 0))
+        return nonce
+
+    # Verificar y actualizar el nonce en la base de datos
+    def check_and_update_nonce(nonce):
+        cursor.execute("SELECT count FROM nonces WHERE nonce=?", (nonce,))
+        result = cursor.fetchone()
+        if result:
+            count = result[0]
+            if count == 0:
+                with conn_db:
+                    cursor.execute("UPDATE nonces SET count=? WHERE nonce=?", (1, nonce))
+                return True
+            else:
+                return False
+        else:
+            return False
+
     print(f"Connected by {addr}")
     with conn:
         while True:
@@ -39,34 +113,53 @@ def handle_client(conn, addr):
 
             received_message = data.decode()
 
+            # Si el cliente solicita un nonce
             if received_message == "nonce_peticion":
                 nonce = generate_nonce()
                 conn.sendall(nonce.encode())
             else:
                 try:
+                    # Decodificar el mensaje JSON
                     json_data = json.loads(received_message)
+                    message = json_data.get('message', '')
+                    hmac_received = json_data.get('hmac', '')
+
+                    # Imprimir el HMAC recibido
+                    print(f"HMAC received from {addr}: {hmac_received}")
+
+                    # Verificar el HMAC
+                    if not verify_hmac(json.dumps({'message': message, 'nonce': json_data.get('nonce')}), hmac_received):
+                        conn.sendall("Invalid HMAC!".encode())
+                        continue
+
+                    # Verificar y actualizar el nonce
                     nonce = json_data.get('nonce', '')
-                    if check_nonce_in_db(nonce):
-                        conn.sendall("Posible Repeat Attack".encode())
+                    if not check_and_update_nonce(nonce):
+                        conn.sendall("Possible Repeat Attack".encode())
                     else:
-                        print(f"Received from {addr}: {received_message}")
+                        # Imprimir el mensaje recibido
+                        print(f"Message received from {addr}: {message}")
                         conn.sendall(data)
                 except json.JSONDecodeError:
                     conn.sendall("Invalid JSON data received".encode())
-                    
-    print(f"Connection with {addr} closed")
 
+    print(f"Connection with {addr} closed")
+    
+    # Cerrar la conexión con la base de datos al finalizar la función
+    conn_db.close()
+
+
+# Iniciar el servidor y esperar conexiones de clientes
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     print(f"Listening on {HOST}:{PORT}")
-    
-    while True:  # Infinite loop to keep server running and listening for new connections
-        conn, addr = s.accept()
 
-        # Start a new thread to handle the client connection
+    # Aceptar conexiones de clientes y manejarlas en hilos separados
+    while True:
+        conn, addr = s.accept()
         client_thread = threading.Thread(target=handle_client, args=(conn, addr))
         client_thread.start()
 
-# No olvides cerrar la conexión a la base de datos cuando ya no la necesites
+# Cerrar la conexión con la base de datos al finalizar la ejecución del programa
 conn.close()
