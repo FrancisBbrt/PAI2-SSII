1d1fd2877c5b4905e99c01bc2ee0d4633f56857f
Gustavo Barros <gustavoasb@gmail.com>
2023-10-22 21:23:18 +0200

    Listo

--- src/clientsocket.py
+++ src/clientsocket.py
@@ -4,6 +4,7 @@ import sqlite3
 import random
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, hmac
+import time
 
 # Definir el HOST y el PORT para la conexión del cliente al servidor
 HOST = "127.0.0.1"
@@ -54,7 +55,11 @@ def generate_random_messages(n):
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
 
-    messages_to_send = generate_random_messages(1000)  # Cambia 10 por el número de mensajes que quieras generar
+    number_messages = 10 # Define el número de mensajes que quieras generar
+    messages_to_send = generate_random_messages(number_messages)
+
+    print(f"\nGenerando {number_messages} messages aleatorias con diferentes comportamientos, 2 segundos para empezar\n")
+    time.sleep(2)
 
     for message in messages_to_send:
         try:
@@ -76,21 +81,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             # Factor de aleatoriedad
             dice = random.randint(1, 6)
             if dice == 1:
-                print("Resultado del dado: 1 - Usando un nonce ya utilizado.")
+                print("Mensaje - Usando un nonce ya utilizado.")
                 used_nonce = get_used_nonce()
                 if used_nonce:
                     data_object['nonce'] = used_nonce
             elif dice == 2:
-                print("Resultado del dado: 2 - Usando un HMAC aleatorio.")
+                print("Mensaje - Usando un HMAC modificado aleatorio.")
                 data_object['hmac'] = "random_hmac_value_{}".format(random.randint(1, 10000))
             elif dice == 3:
-                print("Resultado del dado: 3 - Usando un nonce ya utilizado y un HMAC aleatorio.")
+                print("Mensaje - Usando un nonce ya utilizado y un HMAC modificado aleatorio.")
                 used_nonce = get_used_nonce()
                 if used_nonce:
                     data_object['nonce'] = used_nonce
                 data_object['hmac'] = "random_hmac_value_{}".format(random.randint(1, 10000))
             else:
-                print(f"Resultado del dado: {dice} - Comportamiento normal.")
+                print(f"Mensaje - Comportamiento normal.")
 
             s.sendall(json.dumps(data_object).encode())
 
@@ -98,7 +103,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 update_nonce_count(nonce_response)
 
             data = s.recv(1024)
-            print(f"Received from server: {data.decode()}")
+            print(f"Received from server: {data.decode()}\n")
+
+            time.sleep(0.5)
 
         except Exception as e:
             print(f"Error: {e}")
